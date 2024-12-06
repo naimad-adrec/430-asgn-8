33d8bd7f6e85dadb2a3603b693e432d42dd11066
briancsp <runnerb2003@gmail.com>
2024-12-06 12:11:38 -0800

    more example test cases for primops

--- src/InterpreterTests.groovy
+++ src/InterpreterTests.groovy
@@ -1,18 +1,74 @@
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import static org.junit.jupiter.api.Assertions.*
 import model.exprc.*
 
 class InterpreterTest {
 
-    @Test
-    void testAddition() {
-        def expr = new AppC(
-                new IdC("+"),
-                [new NumC(3), new NumC(4)]
-        )
+    @Nested
+    @DisplayName("Primops Tests")
+    class testPrimops {
+        @Test
+        void testAdd() {
+            def expr = new AppC(
+                    new IdC("+"),
+                    [new NumC(3), new NumC(4)]
+            )
 
-        def result = Interpreter.topInterp(expr)
+            def result = Interpreter.topInterp(expr)
 
-        assertEquals("7", result)
+            assertEquals("7", result)
+        }
+
+        @Test
+        void testSub() {
+            def expr = new AppC(
+                    new IdC("-"),
+                    [new NumC(3), new NumC(4)]
+            )
+
+            def result = Interpreter.topInterp(expr)
+
+            assertEquals("-1", result)
+        }
+
+        @Test
+        void testMult() {
+            def expr = new AppC(
+                    new IdC("*"),
+                    [new NumC(3), new NumC(4)]
+            )
+
+            def result = Interpreter.topInterp(expr)
+
+            assertEquals("12", result)
+        }
+
+        @Test
+        void testDiv() {
+            def expr = new AppC(
+                    new IdC("/"),
+                    [new NumC(3), new NumC(4)]
+            )
+
+            def result = Interpreter.topInterp(expr)
+
+            assertEquals("0.75", result)
+        }
+
+        @Test
+        void testDivByZero() {
+            def expr = new AppC(
+                    new IdC("/"),
+                    [new NumC(3), new NumC(0)]
+            )
+
+            RuntimeException exception = assertThrows(RuntimeException.class,
+                    {Interpreter.topInterp(expr)})
+
+            assertEquals("AAQZ divide by 0", exception.getMessage())
+        }
     }
+
 }
